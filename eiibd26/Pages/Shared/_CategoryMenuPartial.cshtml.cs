34af42ff1ae9ea8feafdf41cca1d4208b82ed6f7
Carlos C. Berletzis <c.berletzis@dological.com>
2026-01-06 20:20:11 -0600

    +++

---
+++ eiibd26/Pages/Shared/_CategoryMenuPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eiibd26.Pages.Shared
+{
+    public class _CategoryMenuPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
