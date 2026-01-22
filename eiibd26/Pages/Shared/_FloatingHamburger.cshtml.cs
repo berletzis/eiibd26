2a2e4e3c73b5c8feac93fcfbea2896e704c5b4ce
Carlos C. Berletzis <c.berletzis@dological.com>
2026-01-21 20:26:15 -0600

    +++

---
+++ eiibd26/Pages/Shared/_FloatingHamburger.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eiibd26.Pages.Shared
+{
+    public class _FloatingHamburgerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
